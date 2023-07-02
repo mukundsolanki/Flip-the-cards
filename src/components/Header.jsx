e3884da3d2ac927ae8fe6a790e4bb57a0098f904
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-02 16:08:20 +0530

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(props) {
+  return (
+    <header className="header">
+      <h4>{props.wins} wins</h4>
+      <h3>Memory Game</h3>
+      <button>New Game</button>
+    </header>
+  )
+}
