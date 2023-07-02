0e102c4f61f8d98a5b0f632fae7d336bc8762e5a
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-02 14:22:52 +0530

    updated the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,21 @@ import React from 'react';
 const Card = (props) => {
 
     return (
-      <div className="card">
-        <div className={props.selected && 'selected'}>
-          <img alt="" src={props.image} className="card-face" />
-  
-          <img
-            alt=""
-            className="card-back"
-            src={'/assets/fireship.png'}
-            onClick={props.onClick}
-          />
+        <div className="card">
+            <div className={props.selected && 'selected'}>
+                {/* Image behind the card */} 
+                <img alt="" src={props.image} className="card-face" />
+
+                {/* Front image of the card */} 
+                <img
+                    alt=""
+                    className="card-back"
+                    src={'/assets/fireship.png'}
+                    onClick={props.onClick}
+                />
+            </div>
         </div>
-      </div>
     );
-  };
-  
-  export default Card;
+};
+
+export default Card;
